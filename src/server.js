9672446afd5d7ce9ead1c83c845b39af1609fba5
iryna-pona <ipona3710@gmail.com>
2026-01-24 02:54:40 +0200

    correction notesController.js, note.js, server.js

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import { errors } from 'celebrate';
 
 import notesRoutes from './routes/notesRoutes.js';
 
@@ -17,6 +18,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(notesRoutes);
+
+app.use(errors());
 
 app.use(notFoundHandler);
 app.use(errorHandler);
